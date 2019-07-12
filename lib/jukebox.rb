5d7d7d330caa87c18361f7181e3472dba8ea1ef9
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-12 18:09:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,38 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands: 
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+end
+
+def play(songs)
+
+end
+
+
+def list(songs)
+  songs.each_with_index do |val, index| 
+    index = index + 1
+  puts "#{index}. #{val}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+
+  
